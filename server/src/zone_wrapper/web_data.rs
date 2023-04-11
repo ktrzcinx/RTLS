d3e228a57325973463b31eb4e04f5ab5ff6ed791
Trzcinski, Karol <karol.trzcinski@intel.com>
2023-04-11 14:26:00 +0200

    server: Do not respond to web_data socket by default

--- server/src/zone_wrapper/web_data.rs
+++ server/src/zone_wrapper/web_data.rs
@@ -8,14 +8,11 @@ use log::info;
 pub fn parse(
     zone: &mut engine::zone::Zone,
     cmd: &MessageFormat,
-    sender: &SharedSender,
+    _: &SharedSender,
 ) -> Option<MessageTarget> {
     match cmd {
         MessageFormat::Text(txt) => info!("zone {} received dev txt data {}", zone.id, txt),
         MessageFormat::Bin(bin) => info!("zone {} received dev bin data {:?}", zone.id, bin),
     }
-    Some(MessageTarget::Direct(
-        MessageFormat::Text("Thanks".to_string()),
-        sender.clone(),
-    ))
+    None
 }
